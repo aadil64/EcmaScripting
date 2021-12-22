6c14e2da9cd51827b7e49abbcc3fc483beecaaf6
Aadil Shaikh <aadik2325@gmail.com>
2021-12-22 22:25:54 +0530

    12/22/2021

---
+++ es/declaration.js
@@ -0,0 +1,26 @@
+// //declaration of object//
+// var object ={
+//     name:"Saqeb",
+//     surname:"Khan"
+// }
+// // Declaration of array
+
+// const a =[1,2,3]
+// //decrarion of undifined datatype//
+// const name;
+
+// //decration of null//
+// const a=null
+// //declaration of function//
+// function news(){
+
+// }
+// //declaration of arrow function//
+// const a=()=>{
+
+// }
+var a = [];
+for(i=0; i<=1000; i++){
+    a.push(i)
+}
+console.log(a);
