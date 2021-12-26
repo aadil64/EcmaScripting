d363410c412aed2d4c14fe80f121ea7b6597eb43
Aadil Shaikh <aadik2325@gmail.com>
2021-12-26 23:58:57 +0530

    Create cfizzbuzzforloop.js

---
+++ es/cfizzbuzzforloop.js
@@ -0,0 +1,12 @@
+function cfizzbuzzforloop(n){
+   for( i=1; i<=n; i++){
+       if (n%3==0 && n%5==0)
+       console.log("fizzBuzz")
+       if (n%3==0)
+       console.log("fizz")
+       if (n%5==0)
+       console.log("buzz")
+       else()
+   }
+}
+cfizzbuzzforloop(15)
